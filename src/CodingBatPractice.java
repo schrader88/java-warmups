4db9170a660b10c5d054515aa23bc9b1b07e53c8
Kyle Schrader <kyle.schrader97@gmail.com>
2021-12-22 11:52:21 -0600

    daily Java warmup

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -1,7 +1,5 @@
 public class CodingBatPractice {
     public static void main(String[] args) {
-        System.out.println(sleepIn(false, false));
-        System.out.println(monkeyTrouble(true, true));
 
     }
 
@@ -32,8 +30,59 @@ public class CodingBatPractice {
         if (a == b) {
             return (a + b) * 2;
         } else {
-            return a * b;
+            return a + b;
         }
     }
+
+
+//    Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+    public int diff21(int n) {
+        if (n > 21) {
+            return (n - 21) * 2;
+        } else {
+            return 21 - n;
+        }
+    }
+
+
+//    We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (hour < 7 || hour > 20) && talking;
+    }
+
+
+//    Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b == 10) {
+            return true;
+        } else if (a + b == 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+//    Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+    public boolean nearHundred(int n) {
+        if (n >= 90 && n <= 110) {
+            return true;
+        } else if (n >= 190 && n <= 210) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+//    Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+//    public boolean posNeg(int a, int b, boolean negative) {
+//
+//    }
 
 }
