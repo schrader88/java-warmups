653a6c9b61a95ff49bab8820fc6d6f213a2d1bd9
Kyle Schrader <kyle.schrader97@gmail.com>
2021-12-27 11:40:56 -0600

    finished morning Java warmup

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -158,7 +158,59 @@ public class CodingBatPractice {
 
 //    Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
 
-//    public boolean or35(int n) {
+    public boolean or35(int n) {
+        return (n % 5 == 0) || (n % 3 == 0);
+    }
+
+
+//    Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+    public String front22(String str) {
+        if (str.length() < 2) {
+            return str + str + str;
+        } else {
+            return str.substring(0,2) + str + str.substring(0,2);
+        }
+    }
+
+
+//    Given a string, return true if the string starts with "hi" and false otherwise.
+
+    public boolean startHi(String str) {
+        if (str.startsWith("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+//    Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+    public boolean icyHot(int temp1, int temp2) {
+        if (temp1 > 100 && temp2 < 0) {
+            return true;
+        } else return temp2 > 100 && temp1 < 0;
+    }
+
+
+//    Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+    }
+
+
+//    We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
+    }
+
+
+//    We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+//    public boolean loneTeen(int a, int b) {
 //
 //    }
 }
