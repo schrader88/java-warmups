dd0104030ce84966d10b5055ca424bf56e6cb15b
Kyle Schrader <kyle.schrader97@gmail.com>
2022-01-20 12:10:17 -0600

    Java technical warmup

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -656,7 +656,22 @@ public class CodingBatPractice {
 
 //    For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "public int round10(int num) {" and call it 3 times. Write the helper entirely below and at the same indent level as roundSum().
 
-//    public int roundSum(int a, int b, int c) {
+    public int roundSum(int a, int b, int c) {
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public int round10(int num) {
+        int a = (num / 10) * 10;
+        int b = a + 10;
+
+        return (num - a >= b - num) ? b : a;
+    }
+
+
+//    Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
+
+//    public boolean closeFar(int a, int b, int c) {
 //
 //    }
+
 }
