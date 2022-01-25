4757fd4eae14d795fc709c94321c81fd2c22d472
Kyle Schrader <kyle.schrader97@gmail.com>
2022-01-25 12:51:45 -0600

    daily Java technical warmups

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -746,7 +746,44 @@ public class CodingBatPractice {
 
 //    The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
 
-//    public int fibonacci(int n) {
+    public int fibonacci(int n) {
+        if ((n == 0) || (n == 1)) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+
+//    We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+    public int bunnyEars2(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        }
+
+        if (bunnies % 2 == 0) {
+            return 3 + bunnyEars2(bunnies - 1);
+        } else {
+            return 2 + bunnyEars2(bunnies - 1);
+        }
+    }
+
+
+//    We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+    public int triangle(int rows) {
+        if (rows == 0) {
+            return 0;
+        }
+
+        return rows + triangle(rows - 1);
+    }
+
+
+//    Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+//    public int sumDigits(int n) {
 //
 //    }
 
