bfecb4616c3fc5e4a00dff20e30492d86d8293e1
Kyle Schrader <kyle.schrader97@gmail.com>
2022-02-08 14:55:07 -0600

    daily technical Java warmup(s)

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -1259,9 +1259,57 @@ public class CodingBatPractice {
     }
 
 
-//    Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
 
-//    public String starOut(String str) {
+//    Given an array of scores, return true if each score is equal or greater than the one before. The array will be length 2 or more.
+
+    public boolean scoresIncreasing(int[] scores) {
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] > scores[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+//    Given an array of scores, return true if there are scores of 100 next to each other in the array. The array length will be at least 2.
+
+    public boolean scores100(int[] scores) {
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] == 100) {
+                if (scores[i] != scores[0] && (scores[i + 1] == 100 || scores[i - 1] == 100)) {
+                    return true;
+                } else if (scores[i] == scores[0] && scores[i + 1] == 100) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+//    Given an array of scores sorted in increasing order, return true if the array contains 3 adjacent scores that differ from each other by at most 2, such as with {3, 4, 5} or {3, 5, 5}.
+
+    public boolean scoresClump(int[] scores) {
+        int absOne;
+        int absTwo;
+
+        for (int i = 0; i < scores.length - 2; i++) {
+            absOne = Math.abs(scores[i] - scores[i + 1]);
+            absTwo = Math.abs(scores[i] - scores[i + 2]);
+            if (absOne <= 2 && absTwo <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+//    Given an array of scores, compute the int average of the first half and the second half, and return whichever is larger. We'll say that the second half begins at index length/2. The array length will be at least 2. To practice decomposition, write a separate helper method
+//    int average(int[] scores, int start, int end) { which computes the average of the elements between indexes start..end. Call your helper method twice to implement scoresAverage(). Write your helper method after your scoresAverage() method in the JavaBat text area. Normally you would compute averages with doubles, but here we use ints so the expected results are exact.
+
+//    public int scoresAverage(int[] scores) {
 //
 //    }
-}
+
+    }
