41fcba34e08dff4c9ce244d536fa615870a66aef
Kyle Schrader <kyle.schrader97@gmail.com>
2022-02-21 11:54:11 -0600

    daily technical Java warmup

--- src/CodingBatPractice.java
+++ src/CodingBatPractice.java
@@ -1535,9 +1535,40 @@ public class CodingBatPractice {
 
 //(A variation on the sumHeights problem.) We have an array of heights, representing the altitude along a walking trail. Given start/end indexes into the array, return the number of "big" steps for a walk starting at the start index and ending at the end index. We'll say that step is big if it is 5 or more up or down. The start end end index will both be valid indexes into the array with start <= end.
 
-//    public int bigHeights(int[] heights, int start, int end) {
+    public int bigHeights(int[] heights, int start, int end) {
+        int bigSteps = 0;
+
+        for (int i = start; i < end; i++) {
+            if (Math.abs(heights[i] - heights[i + 1]) >= 5) {
+                bigSteps++;
+            }
+        }
+        return bigSteps;
+    }
+
+
+//    We have data for two users, A and B, each with a String name and an int id. The goal is to order the users such as for sorting. Return -1 if A comes before B, 1 if A comes after B, and 0 if they are the same. Order first by the string names, and then by the id numbers if the names are the same. Note: with Strings str1.compareTo(str2) returns an int value which is negative/0/positive to indicate how str1 is ordered to str2 (the value is not limited to -1/0/1). (On the AP, there would be two User objects, but here the code simply takes the two strings and two ints directly. The code logic is the same.)
+
+    public int userCompare(String aName, int aId, String bName, int bId) {
+        if (aName.compareTo(bName) < 0) {
+            return -1;
+        } else if (aName.compareTo(bName) > 0) {
+            return 1;
+        } else if (aId < bId) {
+            return -1;
+        } else if (aId > bId) {
+            return 1;
+        }
+        return 0;
+    }
+
+
+//    Start with two arrays of strings, A and B, each with its elements in alphabetical order and without duplicates. Return a new array containing the first N elements from the two arrays. The result array should be in alphabetical order and without duplicates. A and B will both have a length which is N or more. The best "linear" solution makes a single pass over A and B, taking advantage of the fact that they are in alphabetical order, copying elements directly to the new array.
+
+//    public String[] mergeTwo(String[] a, String[] b, int n) {
 //
 //    }
+
 
 
 
